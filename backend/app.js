f3f49a98430894743cea2de15400fa8595bd9ffd
Rasmus Eskola <fruitiex@gmail.com>
2015-05-13 15:35:24 +0300

    show a placeholder until apidoc is generated

--- backend/app.js
+++ backend/app.js
@@ -14,12 +14,15 @@ var l = winston.loggers.get('default');
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var fs = require('fs');
 
 mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/civis');
 var db = mongoose.connection;
 
 var app = express();
 app.set('mongoose', mongoose);
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/apidoc');
 
 var port = process.env.PORT || 3000;
 
@@ -27,6 +30,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(require('./middlewares/auth').initialize());
 app.use('/api', require('./routes'));
+app.get('/', function(req, res, next) {
+  if (fs.existsSync(__dirname + '/apidoc/index.html')) {
+    next();
+  } else {
+    res.render('placeholder');
+  }
+});
 app.use(express.static(__dirname + '/apidoc'));
 
 db.on('error', l.error.bind(console, 'connection error:'));
