45306b0091a458003b2921314a510c3ef245b31c
Rasmus Eskola <fruitiex@gmail.com>
2015-06-09 13:17:02 +0300

    include challenge and community models in models/index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 exports.action = require('./action');
+exports.challenge = require('./challenge');
+exports.community = require('./community');
 exports.consumption = require('./consumption');
 exports.household = require('./household');
 exports.user = require('./user');
