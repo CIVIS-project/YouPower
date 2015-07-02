1a1dad2fcbe967cdf9b5c5e383066ee7dc5b5036
tahakachwala <tahakachwala@wlnss.com>
2015-07-02 14:35:02 +0300

    Facebook connect for existing accounts DONEgit add -A

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -44,7 +44,7 @@ exports.initialize = function() {
     });
   }));
 
-  if (!process.env.FACEBOOK_APP_ID || !process.env.FACEBOOK_APP_SECRET) {
+  /*if (!process.env.FACEBOOK_APP_ID || !process.env.FACEBOOK_APP_SECRET) {
 //    if (!process.env.FACEBOOK_APP_ID) {
     l.warn('Facebook login not set up! Please set environment variables:');
     l.warn('FACEBOOK_APP_ID');
@@ -52,6 +52,7 @@ exports.initialize = function() {
     l.warn('FACEBOOK_CALLBACK_URL');
     l.warn('Disabling Facebook login.');
   } else {
+
     passport.use(new FacebookStrategy({
       clientID: process.env.FACEBOOK_APP_ID,
       clientSecret: process.env.FACEBOOK_APP_SECRET,
@@ -102,20 +103,100 @@ exports.initialize = function() {
       });
       
     }));
-    passport.use('facebook-authz', new FacebookStrategy({
+   
+  }*/
+  passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
-      clientSecret: process.env.FACEBOOK_APP_SECRET,
-      callbackURL: process.env.FACEBOOK_CALLBACK_URL ||
-        'http://localhost:3000/api/auth/facebook/connectfb',
-      enableProof: false,
-      passReqToCallback: true
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/api/auth/facebook/callback",
+    enableProof: false
   },
-  function(req,accessToken, refreshToken, profile, done) {
-    console.log("REJOICE",req.user);
+  function(accessToken, refreshToken, profile, done) {
+    //console.log("profile",profile);
+    //console.log("profile",profile);
+        process.nextTick(function () {
+        User.find({facebookId: profile.id}, false, null, null, function(err, user) {
+            if (err) {
+                      return done(err);
+                      } 
+
+            else if(user) {
+                  console.log("From facebook/callback");
+                  return done(err, user);
+                }
+            else{
+          
+            // TODO: refactor this mess
+            // user does not exist, register new user    
+            crypto.randomBytes(48, function(ex, buf) {
+              var password = buf.toString('hex');
+              //console.log('profile',profile);        
+              User.register({
+                // TODO: get email via facebook
+                email: profile.emails[0].value,
+                //email: mongoose.Types.ObjectId(),
+                facebookId: profile.id,
+                profile: {
+                  name: profile.displayName,
+                  gender: profile.gender
+                }
+              }, password, function(err,user) {
+                if (err) {
+                  return done(err);
+                }
+                console.log("\n");
+                console.log("data",user);
+                return done(null,user);
+              
+              });
+            });
+            }
+
+            
+            });
+
+    });
   }
 ));
-  }
 
+
+//Code for connecting fb account with existing account
+passport.use('facebook-authz', new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/api/auth/facebook/callbackfb"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    console.log("Authorization success profile",profile);
+    
+
+    User.find({token: "3b6a60944460403f077312fc6989c592e32176d40422415cc811a6c284549fb076e680e997632720a54b8689f602f776"}, false, null, null, function(err, user) {
+            if (err) {
+                      console.log("ERROR");
+                      return done(err);
+                      } 
+
+            else if(!user) {
+                  console.log("The given token does not exist for the user");
+                  return done(err, "The given user does not exist");
+                }
+            else{
+                console.log("USER",user);
+                user.facebookId  = profile.id;
+                user.profile.gender   = profile.gender;
+
+                user.markModified('profile.gender');
+                user.markModified('facebookId');
+                user.save();
+                //console.log("USERUPDATED",user);
+                return done(null,"Facebook account successfully connected");
+          
+            }
+
+            
+            });
+  }
+));
 
   passport.serializeUser(User.serializeUser());
   passport.deserializeUser(User.deserializeUser());
@@ -132,13 +213,21 @@ exports.authenticate = function() {
 };
 
 exports.facebook = function() {
-  return passport.authenticate('facebook', { scope: ['email'],session: false});
+ return function(req,res)
+  {
+    auth.passport("facebook",{session: false})
+    console.log(req.user);
+  };
 };
-
+exports.facebookcb = function() {
+  return passport.authenticate('facebook', {session: false});
+};
 exports.connectFb = function(user) {
   console.log("connectFB fn");
   
-  return passport.authorize('facebook-authz', { scope: ['email'],session: false});
+  return passport.authorize('facebook-authz', { scope: ['email'],session: false},function(req,res){
+    console.log("I hope this works");
+  });
 // return passport.authenticate('facebook', { scope: ['email'],session: false});
   //return passport.authenticate('facebook', { scope: ['email'],session: false});
 };
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,31 +3,48 @@
 var express = require('express');
 var auth = require('../middleware/auth');
 var router = express.Router();
+var passport = require('passport');
 
 /**
  * @api {get} /auth/facebook Redirect to Facebook login
  * @apiGroup Facebook Login
  */
-router.get('/facebook', auth.facebook(), function() {
-	
-  // user is redirected to facebook, do nothing here
+router.get('/facebook',passport.authenticate('facebook',{scope:['user_friends','email'],session: false}),function(req, res){
 });
 
-router.get('/facebook/connect', auth.connectFb("test"), function() {
-	
-  // user is redirected to facebook, do nothing here
-});
 /**
  * @api {get} /auth/facebook/callback Callback URL for Facebook login
  * @apiGroup Facebook Login
  */
-router.get('/facebook/callback', auth.facebook(), function(req, res) {
-	
-  auth.newUserToken(req.user, function(err, token) {
+router.get('/facebook/callback',passport.authenticate('facebook',{session: false}),function(req, res) {
+console.log("success"); 
+auth.newUserToken(req.user, function(err, token) {
     res.successRes(err, {
       token: token
     });
   });
+});
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+
+/**
+ * @api {get} /auth/facebookc Connecting existing accounts with fb
+ * @apiGroup Facebook Login
+ */
+router.get('/facebookc',passport.authorize('facebook-authz',{scope:'email',session: false}),function(req, res){
+});
+
+/**
+ * @api {get} /auth/facebook/callbackfb Callback URL for Facebook login for connection
+ * @apiGroup Facebook Login
+ */
+router.get('/facebook/callbackfb',passport.authorize('facebook-authz',{session: false}),function(req, res) {
+	
+res.successRes(null, {
+      Status: req.account
+    });
 });
 
 module.exports = router;
