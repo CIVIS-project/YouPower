5eb4c08711854c9294834dcabd99f00fa2985db8
CodeTaha <taha.kachwala21@gmail.com>
2015-07-13 16:38:06 +0300

    re committing due to failure of Travis

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -10,7 +10,6 @@ var BearerStrategy = require('passport-http-bearer');
 var BasicStrategy = require('passport-http').BasicStrategy;
 var FacebookStrategy = require('passport-facebook');
 var User = require('../models').users;
-var FB = require('fb');
 
 exports.genToken = function(cb) {
   crypto.randomBytes(48, function(ex, buf) {
