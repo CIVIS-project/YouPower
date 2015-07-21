db853e25bd9ee968873f339f90240469826a0937
Rasmus Eskola <fruitiex@gmail.com>
2015-07-21 14:36:28 +0300

    fix build

--- backend/app.js
+++ backend/app.js
@@ -60,8 +60,8 @@ app.use(function(req, res, next) {
 
 // enable CORS
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With, Authorization");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Authorization');
   next();
 });
 
