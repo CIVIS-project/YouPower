42557b5fc4ab66c73755ce2387e87a3c6cae7b8a
Srinivaasan Gayathri <srinivg1@ws-192.cs.hut.fi>
2015-07-29 13:22:38 +0300

    ALlowing content type in http response headers

--- backend/app.js
+++ backend/app.js
@@ -61,7 +61,7 @@ app.use(function(req, res, next) {
 // enable CORS
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Authorization');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
   next();
 });
 
