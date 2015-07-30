998335f9b131b00989b1dde745d04172a1b0d338
Srinivaasan Gayathri <srinivg1@ws-192.cs.hut.fi>
2015-07-30 11:51:58 +0300

    Dummy data script for household and community

--- backend/models/defaults.js
+++ backend/models/defaults.js
@@ -1,8 +1,10 @@
 'use strict';
-
+var mongoose = require('mongoose');
+var newId = mongoose.Types.ObjectId;
 // default data that can be used to get started from an empty db
 
 exports.user = {
+  _id: newId(),
   email: 'civisuser@test.com',
   name: 'Default Test User',
   token: '0',
@@ -21,5 +23,55 @@ exports.actions = [
     description: 'Find and seal up leaks',
     impact: 3,
     effort: 4
+  }
+];
+
+exports.communities = [
+  {
+    name: 'dummy community 1',
+    members: [
+      exports.user._id
+    ],
+    ownerId: exports.user._id,
+    date:new Date()
+  },
+  {
+    name: 'dummy community 2',
+    members: [
+      exports.user._id
+    ],
+    ownerId: exports.user._id,
+    date:new Date()
+  }
+];
+
+exports.households = [
+  {
+    apartmentId: 1234,
+    address: 'my address',
+    householdType: 'apartment',
+    householdSize: '20m2',
+    familyComposition: {
+      NumAdults: 2,
+      NumKids: 0
+    },
+    energyVal: '42',
+    members: [
+      exports.user._id
+    ]
+  },
+  {
+    apartmentId: 1235,
+    address: 'my address 2',
+    householdType: 'house',
+    householdSize: '40m2',
+    familyComposition: {
+      NumAdults: 2,
+      NumKids: 1
+    },
+    energyVal: '43',
+    members: [
+      exports.user._id
+    ]
   }
 ];
