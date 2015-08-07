3e5422e2c1e1caede4cc42c7ff2e0a0cbddbed06
Rasmus Eskola <fruitiex@gmail.com>
2015-08-07 11:25:47 +0300

    list http methods separately

--- backend/app.js
+++ backend/app.js
@@ -61,7 +61,7 @@ app.use(function(req, res, next) {
 // enable CORS
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
   next();
 });
