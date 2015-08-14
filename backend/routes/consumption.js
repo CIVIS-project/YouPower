f3205408d95c168e5b3da5d90b916ce412ccbd63
Rasmus Eskola <fruitiex@gmail.com>
2015-08-14 09:26:27 +0300

    dummy last consumption reading call

--- backend/routes/consumption.js
+++ backend/routes/consumption.js
@@ -37,6 +37,32 @@ router.get('/', auth.authenticate(), function(req, res) {
 });
 
 /**
+ * @api {get} /consumption/last Last reading of consumption data
+ * @apiGroup Consumption
+ *
+ * @apiParam {String} userId
+ * @apiParam {String} token
+ *
+ * @apiExample {curl} Example usage
+ *  # Get API token via /api/user/token
+ *  export API_TOKEN=fc35e6b2f27e0f5ef...
+ *  curl -i -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $API_TOKEN" -d \
+ *  '{
+ *    "userId": "dummy",
+ *    "token": "dummy"
+ *  }' \
+ *  http://localhost:3000/api/consumption/last
+ *
+ * @apiSuccessExample {[json]} Success-Response:
+ * {
+ *   consumption: 0.9164172718301415,
+ * }
+ */
+router.get('/last', auth.authenticate(), function(req, res) {
+  res.successRes(null, Math.random());
+});
+
+/**
  * @api {get} /consumption/tou Time of Usage
  * @apiGroup Consumption
  *
