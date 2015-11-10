384e8b50c9ae0202265cd0b41fcf63db4605f0f3
Filip Kis <filip.kis@gmail.com>
2015-11-10 15:35:21 +0100

    Delete ProsumptionCtrl.js
    Fixing duplicate file

--- frontend/www/app/prosumption/ProsumptionCtrl.js
+++
@@ -1,6 +0,0 @@
-angular.module('civis.youpower.prosumption').controller('prosumptionCtrl', prosumptionCtrl);
-
-function prosumptionCtrl($scope, $stateParams, $state, User) {
-	//just loads the content of the window once the tabs have been generated
-$state.go('main.actions.yours');
-};
